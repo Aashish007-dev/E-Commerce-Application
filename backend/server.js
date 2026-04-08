e22612277ab5e30cc2328ad7b01c56d381dc73b5
Aashish Chotaliya <aashishchotaliya2003@gmail.com>
2026-04-09 00:26:17 +0530

    intialize backend structure and create model for use and product and connnect a mongodb database

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+connectDB();
+connectCloudinary();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('API Working!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
