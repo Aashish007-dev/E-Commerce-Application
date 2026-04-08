768659c0e3a1caf769ad352fb8456fc4693adb21
Aashish Chotaliya <aashishchotaliya2003@gmail.com>
2026-04-08 12:12:36 +0530

    Complete About us page UI

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/assets'
+import NewsletterBox from '../components/NewsletterBox'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <div className='text-2xl text-center pt-8 border-t'>
+        <Title text1={'ABOUT'} text2={'US'}/>
+      </div>
+
+      <div className='my-10 flex flex-col md:flex-row gap-16'>
+        <img src={assets.about_img} className='w-full md:max-w-[450px]' alt="" />
+        <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius debitis pariatur at, doloremque nesciunt autem placeat delectus animi eos, quod saepe laudantium nisi nostrum neque! Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil nam ex et adipisci laborum eos officia atque, ratione doloribus repudiandae!</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus temporibus asperiores, vel obcaecati quae officia nisi sapiente culpa veritatis, quas quisquam facere alias ab? Aliquam similique recusandae saepe cumque hic. Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, esse?</p>
+          <b className='text-gray-800'>Our Mission</b>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur incidunt illum dolores hic? Dolore, sequi optio sed ea doloribus aliquid minima exercitationem reiciendis autem ab. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis minima aliquam repellat iste quisquam. Nobis?</p>
+        </div>
+      </div>
+
+      <div className='text-xl py-4'>
+        <Title text1={'WHY'} text2={'CHOOSE US'}/>
+      </div>
+
+      <div className='flex flex-col md:flex-row text-sm mb-20'>
+          <div className='border border-gray-200 px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+            <b>Quality Assurance:</b>
+            <p className='text-gray-600'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto commodi nisi sunt nemo possimus qui aliquam dolor. Assumenda similique a nesciunt nulla voluptatibus fugit, accusamus aliquid blanditiis minus alias nemo!</p>
+          </div>
+
+          <div className='border border-gray-200 px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+            <b>Convenience:</b>
+            <p className='text-gray-600'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto commodi nisi sunt nemo possimus qui aliquam dolor. Assumenda similique a nesciunt nulla voluptatibus fugit, accusamus aliquid blanditiis minus alias nemo!</p>
+          </div>
+
+          <div className='border border-gray-200 px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+            <b>Exceptional Customer Service:</b>
+            <p className='text-gray-600'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto commodi nisi sunt nemo possimus qui aliquam dolor. Assumenda similique a nesciunt nulla voluptatibus fugit, accusamus aliquid blanditiis minus alias nemo!</p>
+          </div>
+      </div>
+      <NewsletterBox />
+    </div>
   )
 }
 
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -9,10 +9,6 @@ const Login = () => {
     
   }
 
-
-
-
-
   return (
     <form onClick={onSubmitHandler} className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4  text-gray-800'>
       <div className='inline-flex items-center gap-2 mb-2 mt-10'>
