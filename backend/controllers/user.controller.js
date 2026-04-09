5109015d40fbc8926c15fcb34d8351222ce580ce
Aashish Chotaliya <aashishchotaliya2003@gmail.com>
2026-04-09 10:47:03 +0530

    create userRoutes and registerController api end point

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,66 @@
+import userModel from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+import validator from "validator";
+import bcrypt from "bcrypt";
+
+
+const createToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET);
+}
+
+
+// User
+const loginUser = async (req, res) => {
+    
+}
+
+
+const registerUser = async (req, res) => {
+    try {
+        const {name, email, password} = req.body;
+
+        const existingUser = await userModel.findOne({email});
+        if(existingUser){
+            return res.json({success: false, message: 'User already exists'});
+        }
+
+        // Validating email & strong password
+        if (!validator.isEmail(email)) {
+            return res.json({success: false, message: 'Invalid email'});
+        }
+
+        if (password.length < 8) {
+            return res.json({success: false, message: 'Password must be at least 8 characters long'});
+        }
+
+        // Hashing password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new userModel({
+            name,
+            email,
+            password: hashedPassword
+        });
+
+        const user = await newUser.save();
+
+        const token = createToken(user._id);
+
+        res.json({success: true, message: 'User registered successfully', token});
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: 'Error registering user', error});
+    }
+}
+
+
+// Admin
+const adminLogin = async (req, res) => {
+
+}
+
+
+
+
+export {loginUser, registerUser, adminLogin};
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import cors from 'cors';
 import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
+import userRouter from './routes/user.route.js';
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,6 +13,9 @@ connectCloudinary();
 
 app.use(cors());
 app.use(express.json());
+
+
+app.use('/api/user', userRouter)
 
 app.get('/', (req, res) => {
     res.send('API Working!');
