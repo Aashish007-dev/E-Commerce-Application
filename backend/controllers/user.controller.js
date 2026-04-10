d73d1d3743605e666f584f8afcaea621cd059608
Aashish Chotaliya <aashishchotaliya2003@gmail.com>
2026-04-10 12:08:32 +0530

    feat: implement user authentication, admin authorization middleware, and product management routes

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -77,7 +77,19 @@ const registerUser = async (req, res) => {
 
 // Admin
 const adminLogin = async (req, res) => {
+    try {
+        const {email, password} = req.body;
 
+        if(email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD){
+            const token = jwt.sign(email+password, process.env.JWT_SECRET);
+            res.json({success: true, message: "Admin logged in successfully", token});
+        }else{
+            res.json({success: false, message: "Invalid credentials"});
+        }
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: "Error logging in admin", error});
+    }
 }
 
 
