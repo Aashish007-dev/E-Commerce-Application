58ffd485dd342bc1d91a35e737dd12538d93fcfe
Aashish Chotaliya <aashishchotaliya2003@gmail.com>
2026-04-14 02:03:13 +0530

    feat: implement order management system with Stripe integration and admin status tracking

--- backend/routes/order.route.js
+++ backend/routes/order.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { allOrders, placeOrder, placeOrderRazorpay, placeOrderStripe, updateStatus, userOrders } from '../controllers/order.controller.js';
+import { allOrders, placeOrder, placeOrderRazorpay, placeOrderStripe, updateStatus, userOrders, verifyStripe } from '../controllers/order.controller.js';
 import authUser  from '../middleware/auth.middleware.js';
 import adminAuth from '../middleware/adminAuth.middleware.js';
 
@@ -20,6 +20,7 @@ orderRouter.post('/razorpay', authUser, placeOrderRazorpay);
 // User Features
 orderRouter.post('/userorders', authUser, userOrders);
 
-
+// verify payment
+orderRouter.post('/verifyStripe', authUser,verifyStripe);
 
 export default orderRouter;
